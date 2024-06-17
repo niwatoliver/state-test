33919cb8ee4e8fb7e8aec92825083636765f52d5
oliver <minakawa.daiki.as@gmail.com>
2024-06-17 22:20:47 +0900

    impl

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import { Button } from "@/components/Button";
 import TestComponent from "@/components/TestComponent";
+import { fetchTest } from "@/actions";
 
-export default function Home() {
+export default async function Home() {
+  const json = await fetchTest();
+  const json2 = await fetchTest();
   return (
     <main>
+      1: {JSON.stringify(json)}
+      2: {JSON.stringify(json2)}
       <TestComponent from="Home" />
       <Button />
     </main>
