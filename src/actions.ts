0e288515640a320232bf16991235f0002c81049d
oliver <minakawa.daiki.as@gmail.com>
2024-06-17 19:49:12 +0900

    impl

--- src/actions.ts
+++ src/actions.ts
@@ -1,15 +1,20 @@
 "use server";
 
+import { hc } from "hono/client";
+import { HonoType } from "@/app/api/[[...route]]/route";
+
 const BACKEND_BASE_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
     : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
 
 export async function fetchTest() {
-  const response = await fetch(`${BACKEND_BASE_URL}/api`, {
-    next: { tags: ["counter-test"] },
-    cache: "no-store",
-  });
-  const json = await response.json();
+  const client = hc<HonoType>(BACKEND_BASE_URL);
+  const hello = await client.api.hello.$get();
+  // const response = await fetch(`${BACKEND_BASE_URL}/api`, {
+  //   next: { tags: ["counter-test"] },
+  //   cache: "no-store",
+  // });
+  const json = await hello.json();
   return json.now;
 }
---
+++ src/app/api/[[...route]]/route.ts
@@ -0,0 +1,16 @@
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+
+export const runtime = "edge";
+
+const app = new Hono().basePath("/api").get("/hello", (c) => {
+  return c.json({
+    message: "Hello Next.js!",
+    now: Date.now(),
+  });
+});
+
+export const GET = handle(app);
+export const POST = handle(app);
+
+export type HonoType = typeof app;
