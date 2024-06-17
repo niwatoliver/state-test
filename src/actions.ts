7607f881c12e8938486b221ba3a36a030b054ebc
oliver <minakawa.daiki.as@gmail.com>
2024-06-17 19:57:24 +0900

    impl

--- src/actions.ts
+++ src/actions.ts
@@ -1,13 +1,20 @@
 "use server";
 
 import { backendClient } from "@/app/backendClient";
+import { notFound } from "next/navigation";
 
 export async function fetchTest() {
   const client = backendClient();
-  const hello = await client.api.hello.$get();
-  // const response = await fetch(`${BACKEND_BASE_URL}/api`, {
-  //   next: { tags: ["counter-test"] },
-  //   cache: "no-store",
-  // });
-  return await hello.json();
+  const res = await client.api.hello.$get();
+
+  if (!res.ok) {
+    return notFound();
+  }
+
+  const json = await res.json();
+  if (json === null) {
+    return notFound();
+  }
+
+  return json;
 }
