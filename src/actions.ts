78753ffa2b55868ea5dd06f3abcd109eab228e11
oliver <minakawa.daiki.as@gmail.com>
2024-06-17 19:52:58 +0900

    impl

--- src/actions.ts
+++ src/actions.ts
@@ -6,15 +6,15 @@ import { HonoType } from "@/app/api/[[...route]]/route";
 const BACKEND_BASE_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
-    : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+    : // : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+      `https://state-test-zeta.vercel.app`;
 
 export async function fetchTest() {
-  // const client = hc<HonoType>(BACKEND_BASE_URL);
-  // const hello = await client.api.hello.$get();
+  const client = hc<HonoType>(BACKEND_BASE_URL);
+  const hello = await client.api.hello.$get();
   // const response = await fetch(`${BACKEND_BASE_URL}/api`, {
   //   next: { tags: ["counter-test"] },
   //   cache: "no-store",
   // });
-  // return await hello.json();
-  return { message: "Hello, World!" };
+  return await hello.json();
 }
