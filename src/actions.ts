3c2c6bc0fbd4882591611e20bc7f53526180517a
oliver <minakawa.daiki.as@gmail.com>
2024-06-17 19:51:49 +0900

    impl

--- src/actions.ts
+++ src/actions.ts
@@ -9,11 +9,12 @@ const BACKEND_BASE_URL =
     : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
 
 export async function fetchTest() {
-  const client = hc<HonoType>(BACKEND_BASE_URL);
-  const hello = await client.api.hello.$get();
+  // const client = hc<HonoType>(BACKEND_BASE_URL);
+  // const hello = await client.api.hello.$get();
   // const response = await fetch(`${BACKEND_BASE_URL}/api`, {
   //   next: { tags: ["counter-test"] },
   //   cache: "no-store",
   // });
-  return await hello.json();
+  // return await hello.json();
+  return { message: "Hello, World!" };
 }
