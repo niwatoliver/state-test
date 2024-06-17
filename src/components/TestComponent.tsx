2c231916519138432b32fafaef7e6f63aa992662
oliver <minakawa.daiki.as@gmail.com>
2024-06-17 22:14:21 +0900

    impl

--- src/components/TestComponent.tsx
+++ src/components/TestComponent.tsx
@@ -2,9 +2,11 @@ import { fetchTest } from "@/actions";
 
 export default async function TestComponent({ from }: { from: string }) {
   const json = await fetchTest();
+  const json2 = await fetchTest();
   return (
     <div>
       {from}: {JSON.stringify(json)}
+      {from}2: {JSON.stringify(json2)}
     </div>
   );
 }
